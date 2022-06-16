5a0631caa84ab943b5fdddaca0089ef8515fb3f7
sattila1999 <somogyiattila417@gmail.com>
2022-06-16 11:40:30 +0200

    story 2 4 12

--- kaland.py
+++ kaland.py
@@ -1,12 +1,4 @@
 while i != 19 or i != 22:
-    if i == 12:
-        print('15. megbököd a vállát és megkínálod cigivel, elfogadja és rágyújt.  \n'
-              '20. sikerül valami nyálas dumát nyomnod a kék szemeiről. \n'
-              '21. elküld a francba.\n')
-        user_input = int(input('Give me a number:'))
-        if user_input == 15 or user_input == 20 or user_input == 21:
-            i = user_input
-
     if i == 2:
         print('észreveszi az éppen érkező töri tanárod, mit tettél és…\n'
               '5. azonnal felrángat az igazgatói irodába. Te hőzöngve tiltakozol végig a folyosón.\n'
@@ -22,5 +14,13 @@ while i != 19 or i != 22:
               '13. kiszúrod az éppen közeledő dögös csajt az évfolyamról és Béci megszólítja.\n')
         user_input = int(('Mit választasz? Írj be egy számot:\n'))
         if user_input == 11 or user_input == 12 or user_input == 13:
+            i = user_input
+
+    if i == 12:
+        print('15. megbököd a vállát és megkínálod cigivel, elfogadja és rágyújt.  \n'
+              '20. sikerül valami nyálas dumát nyomnod a kék szemeiről. \n'
+              '21. elküld a francba.\n')
+        user_input = int(input('Give me a number:'))
+        if user_input == 15 or user_input == 20 or user_input == 21:
             i = user_input
 
