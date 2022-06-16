73f8e32399fe846c9730e28c60cb316d4ba4e1a7
lutzoliver <oliverlutz98@gmail.com>
2022-06-16 10:42:28 +0200

    user input

--- main.py
+++ main.py
@@ -19,3 +19,19 @@ else:  ## barmikor maskor
 
 print('end')
 print('helo')
+
+# user_input = input("adj meg egy számot:")
+# print(type(user_input))
+# print(user_input)
+
+user_input = int(input("adj meg egy számot:"))
+print(user_input)
+print(type(user_input))
+
+flag = True
+counter = 0
+while flag:
+    print('1')
+    counter+=1
+    if counter == 5:
+        flag = False
