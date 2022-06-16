683368aca0c7caf239d6ce10cde864f78ab9f520
powerslave354 <powerslave354@gmail.com>
2022-06-16 11:40:06 +0200

    feladat 11

---
+++ kaland.py
@@ -0,0 +1,8 @@
+    if i == 11:
+        print('19. A legközelebbi kocsmáig meg se állsz és piszkosul berúgsz \n'
+              '3. felrúgod a bejárat melletti szemeteskukát és mellé pöccinted a csikket. \n'
+              '2. elnyomod a csikket az igazgatónő bringájának kerekébe. \n')
+        user_input = int(input('Give me a number:'))
+        if user_input == 19 or user_input == 3 or user_input == 2:
+            i = user_input
+
