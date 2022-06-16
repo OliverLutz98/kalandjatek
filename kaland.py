94bd15e4e55839635f320ccc3f632798ec4acba5
Szabo Kristof <kristof89szabo@gmail.com>
2022-06-16 11:40:45 +0200

    story 1-2

--- kaland.py
+++ kaland.py
@@ -1,10 +1,12 @@
 while i != 19 or i != 22:
-    if i == 12:
-        print('15. megbököd a vállát és megkínálod cigivel, elfogadja és rágyújt.  \n'
-              '20. sikerül valami nyálas dumát nyomnod a kék szemeiről. \n'
-              '21. elküld a francba.\n')
+
+    if i == 1:
+        print('Te vagy az iskola rosszfiúja. Késve érkezel a suli elé, még elszívod a cigidet, aztán… \n'
+              '2. elnyomod a csikket az igazgatónő bringájának kerekébe. \n'
+              '3. felrúgod a bejárat melletti szemeteskukát és mellé pöccinted a csikket.  \n'
+              '4. odaér a haverod, Béci is. Rágyújtotok egy újabb cigire. \n')
         user_input = int(input('Give me a number:'))
-        if user_input == 15 or user_input == 20 or user_input == 21:
+        if user_input == 2 or user_input == 3 or user_input == 4:
             i = user_input
 
     if i == 2:
@@ -16,6 +18,14 @@ while i != 19 or i != 22:
         if user_input == 5 or user_input == 6 or user_input == 7:
             i = user_input
 
+    if i == 12:
+        print('15. megbököd a vállát és megkínálod cigivel, elfogadja és rágyújt.  \n'
+              '20. sikerül valami nyálas dumát nyomnod a kék szemeiről. \n'
+              '21. elküld a francba.\n')
+        user_input = int(input('Give me a number:'))
+        if user_input == 15 or user_input == 20 or user_input == 21:
+            i = user_input
+
     if i == 14:
         print('19. a legközelebbi kocsmáig meg se állsz és piszkosul berúgsz. \n'
               '21. elküld a francba.\n'
@@ -24,5 +34,3 @@ while i != 19 or i != 22:
     user_input = int(input("adj meg egy számot:"))
     if user_input == 19 or user_input == 21 or user_input == 12:
         i = user_input
-
-
