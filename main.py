106a4614e57efb7fc0cf6f928dffb0fe5aa1aafe
lutzoliver <oliverlutz98@gmail.com>
2022-06-16 10:15:10 +0200

    update

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+my_text = 'some text'
+my_number = 5
+my_float = 3.2
+my_bol = False
+# comment
+"""
+another comment
+"""
 
+my_num = 1
+if my_num == 1:
+    print('my number 2')
+elif my_number == 3:
+    print('my number 3')
+else:
+    print('another number')
+
+print('end')
