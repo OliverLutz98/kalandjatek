d5e87dc8c97b47a0ba9892eaee125c72476078c4
dbalazs97 <davidbalazs1997@gmail.com>
2022-06-16 11:33:37 +0200

    5. ág

---
+++ kaland.py
@@ -0,0 +1,9 @@
+
+    if i == 5:
+        print(
+            '10. pechedre akkor ér oda melléd az a dögös csaj az évfolyamról, akinek szemlátomást nem jön be a viselkedésed. \n'
+            '12. kiszúrod az éppen közeledő dögös csajt az évfolyamról és megszólítod. \n'
+            '14. kihúzod magad a tanár kezei közül és röhögve elfutsz előle a folyosón. \n')
+        user_input = int(input('Give me a number:\n'))
+        if user_input == 10 or user_input == 12 or user_input == 14:
+            i = user_input
