2a0fa28e56a9841510bd831de5987427ec071bb6
powerslave354 <powerslave354@gmail.com>
2022-06-16 11:58:52 +0200

    feladat 11

--- kaland.py
+++ kaland.py
@@ -48,7 +48,7 @@ while i != 19 or i != 22:
     if i == 9:
         print('15. megbököd a vállát és megkínálod cigivel, elfogadja és rágyújt.\n'
               '16. Bemész a suliba. Feszkós vagy, ezért betörsz egy ablakot a folyosón.\n'
-              '4. odaér a haverod, Béci is. Rágyújtotok egy újabb cigire.\n')
+              '4. odaér a haverod, Béci is. Rágyújtotok egy újabb cigire..\n')
         user_input = int(input('Adj meg a számot:\n'))
         if user_input == 15 or user_input == 16 or user_input ==4:
             i = user_input
