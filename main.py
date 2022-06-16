ae87be613937943fbb69c68805829f9c188b1d6d
mauditore <maczko97@gmail.com>
2022-06-16 10:13:52 +0200

    update

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+my_text = 'some text'
+my_number = 5
+a = 3.2
+my_float = 3.2
+my_bol = False
+# True
+# My Comment
+"""
+another comment
+"""
 
+my_num = 1
+if my_num == 1:
+    print('my number 2')
+elif my_num == 3:
+    print('my number 3')
+else:  ## barmikor maskor
+    print ('another nuber')
+
+
+print ('end')
