3169ba10b038f9cd822adfff4fd033f401c894fc
laso <koberlinglaszlo@gmail.com>
2022-06-16 10:14:07 +0200

    update

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+my_text = 'some text'
+my_number = 5
+a = 3.2
+my_bol = False
+# True
+# My comment
+"""
+another comment
+"""
 
+my_num = 5
+if my_num == 1:
+    print('my number 2')
+elif my_num ==3:
+    print('my number 3')
+else:
+    print('another number')
+
+print('end')
