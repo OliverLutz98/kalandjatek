8b2b82c048b0387989b799f9077392cb00b09d75
Szabo Kristof <kristof89szabo@gmail.com>
2022-06-16 12:00:14 +0200

    story 1-2

--- kaland.py
+++ kaland.py
@@ -39,13 +39,22 @@ while i != 19 or i != 22:
         if user_input == 4 or user_input == 15 or user_input == 16:
             i = user_input
 
-    if i != 8:
+    if i == 8:
         print('4. odaér a haverod, Béci is. Rágyújtotok egy újabb cigire. \n'
               '17. eltűnsz a helyszínről, mintha semmi közöd se lenne a történtekhez.'
               '18. elégedetten szemléled a művedet. \n')
         user_input = int(input('Mit választasz? Írd be a számot:\n'))
         if user_input == 4 or user_input == 17 or user_input == 18:
             i = user_input
+
+    if i == 9:
+        print('15. megbököd a vállát és megkínálod cigivel, elfogadja és rágyújt.\n'
+              '16. Bemész a suliba. Feszkós vagy, ezért betörsz egy ablakot a folyosón.\n'
+              '4. odaér a haverod, Béci is. Rágyújtotok egy újabb cigire.\n')
+        user_input = int(input('Adj meg a számot:\n'))
+        if user_input == 15 or user_input == 16 or user_input == 4:
+            i = user_input
+
     if i == 12:
         print('15. megbököd a vállát és megkínálod cigivel, elfogadja és rágyújt.  \n'
               '20. sikerül valami nyálas dumát nyomnod a kék szemeiről. \n'
@@ -54,25 +63,15 @@ while i != 19 or i != 22:
         if user_input == 15 or user_input == 20 or user_input == 21:
             i = user_input
 
-    if i == 9:
-        print('15. megbököd a vállát és megkínálod cigivel, elfogadja és rágyújt.\n'
-              '16. Bemész a suliba. Feszkós vagy, ezért betörsz egy ablakot a folyosón.\n'
-              '4. odaér a haverod, Béci is. Rágyújtotok egy újabb cigire.\n')
-        user_input = int(input('Adj meg a számot:\n'))
-        if user_input == 15 or user_input == 16 or user_input ==4:
-            i = user_input
-
     if i == 13:
         print('16. Bemész a suliba. Feszkós vagy, ezért betörsz egy ablakot a folyosón.\n'
               '3. felrúgod a bejárat melletti szemeteskukát és mellé pöccinted a csikket.\n '
               '2. elnyomod a csikket az igazgatónő bringájának kerekébe.\n')
-    if user_input == 16 or user_input == 3 or user_input == 2:
-        i = user_input
-
+        if user_input == 16 or user_input == 3 or user_input == 2:
+            i = user_input
 
     if i == 14:
         print('19. a legközelebbi kocsmáig meg se állsz és piszkosul berúgsz. \n'
               '21. elküld a francba.\n'
               '12. kiszúrod az éppen közeledő dögös csajt az évfolyamról és megszólítod.\n'
               'Mit választasz? Írj be a számot:\n')
-
